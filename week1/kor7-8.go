392a437d9ad6d4454c1776a18a5d702819d8b738
Teerapan Lerdtanaseangtham <garfild5512@gmail.com>
2024-12-10 22:59:01 +0700

    update dynamic n

--- week1/kor7-8.go
+++ week1/kor7-8.go
@@ -3,28 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("kor7: ")
-	kor7()
-	fmt.Println("-----------------")
-	fmt.Println("kor8: ")
-	kor8()
+	selection := 0
+	x := 0
+	fmt.Println("1. kor7")
+	fmt.Println("2. kor8")
+	fmt.Scanf("%d", &selection)
+	if selection == 1 {
+		fmt.Scanf("%d", &x)
+	}
+	n := 0
+	fmt.Scanf("%d", &n)
+	A := []int{}
+	for i := 0; i < n; i++ {
+		var temp int
+		fmt.Scanf("%d", &temp)
+		A = append(A, temp)
+	}
+	if selection == 1 {
+		kor7(x, A)
+	} else {
+		kor8(A)
+	}
 }
 
-func kor7() {
-	x := 20
-	A := []int{5, 15, -30, 10, -5, 40, 10}
+func kor7(x int, A []int) {
+	// A := []int{5, 15, -30, 10, -5, 40, 10}
 	length := len(A)
 	for i := 0; i < length; i++ {
 		for j := i + 1; j < length; j++ {
 			if A[i]+A[j] == x {
-				fmt.Println(A[i], A[j])
+				fmt.Println("=", A[i], A[j])
 			}
 		}
 	}
 }
 
-func kor8() {
-	A := []int{5, 15, -30, 10, -5, 40, 10}
+func kor8(A []int) {
+	// A := []int{5, 15, -30, 10, -5, 40, 10}
 	length := len(A)
 	maxSum := A[0]
 	start := 0
